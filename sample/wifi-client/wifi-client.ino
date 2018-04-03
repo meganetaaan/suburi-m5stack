63828e719e3040ae5418fd28aed3c94eec97c73e
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-04-04 01:27:54 +0900

    Connect to IFTTT

--- sample/wifi-client/wifi-client.ino
+++ sample/wifi-client/wifi-client.ino
@@ -1,6 +1,14 @@
 #include <M5Stack.h>
-#include <WiFi.h>
 #include <string.h>
+#include <WiFi.h>
+#include <ssl_client.h>
+#include <WiFiClientSecure.h>
+#include "const.h"
+
+const char* host = "maker.ifttt.com";
+const char* event = "m5stack";
+const int httpsPort = 443;
+WiFiClientSecure client;
 
 String TFReadFile(String path)
 {
@@ -50,8 +58,8 @@ int cntChrs(String str, char c)
 
 bool configWifi2()
 {
-  char *ssid_ = strToChar("mywifi");
-  char *pswd_ = strToChar("mypasswd");
+  char *ssid_ = strToChar(ssid);
+  char *pswd_ = strToChar(password);
   if (WiFi.begin(ssid_, pswd_))
   {
     delay(10);
@@ -107,22 +115,70 @@ bool configWifi()
 void setup()
 {
   M5.begin();
-  if(configWifi2())
+  while (true)
   {
-    M5.Lcd.setCursor(130, 70);
-    M5.Lcd.print("success");
+    if (configWifi2())
+    {
+      M5.Lcd.setCursor(130, 70);
+      Serial.println("success");
+      M5.Lcd.print("success");
+      break;
+    }
+    else
+    {
+      M5.Lcd.setCursor(130, 70);
+      Serial.println("fail");
+      M5.Lcd.print("fail. retry within 5 seconds...");
+      delay(5000);
+      M5.Lcd.clear();
+    }
   }
-  else
-  {
-    M5.Lcd.setCursor(130, 70);
-    M5.Lcd.print("fail");
+}
+
+void trigger(String val1)
+{
+  Serial.println("host connection");
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("host connection failed");
+    return;
+  }
+  Serial.println("host connection success");
+  String url = "/trigger/";
+  url += event;
+  url += "/with/key/";
+  url += secretKey;
+  url += "?value1=";
+  url += val1;
+
+  String request = "GET " + url + " HTTP/1.1\r\n"
+  + "Host: " + host + "\r\n"
+  + "Connection: close\r\n\r\n";
+
+  Serial.println("making request: " + request);
+  client.print(request);
+
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    Serial.println(line);
+    if (line == "\r")
+    {
+      break;
+    }
   }
 }
 
 void loop()
 {
   M5.update();
-  if(M5.BtnA.wasPressed())
+  if (M5.BtnA.wasPressed())
+  {
+    trigger("A");
+  }
+  if (M5.BtnB.wasPressed())
+  {
+    trigger("B");
+  }
+  if (M5.BtnC.wasPressed())
   {
     M5.Lcd.clear();
     M5.Lcd.setCursor(130, 70);
